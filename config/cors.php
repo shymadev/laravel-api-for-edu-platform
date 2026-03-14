f066400f4deca570f3c55a0e7ad303c6ad0337c7
legalitysh <legalitysh@gmail.com>
2026-03-14 21:41:38 +0300

    feat: optimize cors politics to improve caching and swagger availability

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*', 'swagger/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Accept', 'Authorization', 'X-XSRF-TOKEN'],
-    'exposed_headers' => [],
-    'max_age' => 0,
+    'exposed_headers' => ['Authorization'],
+    'max_age' => 86400,
     'supports_credentials' => true,
 ];
