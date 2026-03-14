fbf6fc8810d8ed063b48d6a02c24ea01ca3538da
legalitysh <legalitysh@gmail.com>
2026-03-14 21:41:56 +0300

    feat: add named route for login

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::get('login', [AuthController::class, 'login'] );
 });
 
 Route::get('phrases', [PhraseController::class, 'index']);
